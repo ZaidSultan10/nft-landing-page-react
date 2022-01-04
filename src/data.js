1aa702bf6739ef4ba53b3fa6d06fe55e9efb1577
Zaid Sultan <zaidsultan09@gmail.com>
2022-01-04 23:45:19 +0400

    initial stage of project

---
+++ src/data.js
@@ -0,0 +1,21 @@
+const data = [{
+    id:1,
+    name:'ciri',
+    image:'daalta hoon',
+    currency:'500 ETH'
+},{
+    id:2,
+    name:'geralt',
+    image:'daalta hoon',
+    currency:'1500 ETH'
+},{
+    id:1,
+    name:'yen',
+    image:'daalta hoon',
+    currency:'800 ETH'
+},{
+    id:1,
+    name:'jaskier',
+    image:'daalta hoon',
+    currency:'2500 ETH'
+}]
