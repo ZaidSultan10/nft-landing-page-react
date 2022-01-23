4a56f9f7ba46da13c6ea9ed57ff73013cb3cbc67
Zaid Sultan <zaidsultan09@gmail.com>
2022-01-23 16:05:55 +0400

    added animation

--- src/components/BodyContent.js
+++ src/components/BodyContent.js
@@ -1,16 +1,33 @@
 import React from 'react'
 import styled from 'styled-components'
+import {InnerLayout} from '../Layouts.js'
+import MainTitle from './MainTitle.js'
+import SellerCards from './SellerCards.js'
 
 const BodyContent = () => {
     return (
         <BodyContentStyled>
-            
+            <InnerLayout>
+                <MainTitle title={'Top Sellers This Month'}
+                subTitle={'All Entreprenuers'} />
+
+                <div className='sellerCards'>
+                    <SellerCards />
+                    <SellerCards />
+                    <SellerCards />
+                </div>
+            </InnerLayout>
         </BodyContentStyled>
     )
 }
 
 const BodyContentStyled = styled.main`
-
+    .sellerCards{
+        display:grid;
+        grid-template-columns:repeat(3,1fr);
+        grid-gap:2rem;
+        margin:2rem 0;
+    }
 `
 
 export default BodyContent
--- src/components/SellerCards.js
+++ src/components/SellerCards.js
@@ -1,17 +1,66 @@
 import React from 'react'
 import styled from 'styled-components'
-
+import data from '../data.js'
 
 const SellerCards = () => {
     return (
         <SellerCardsStyled>
-            
+            {data.map((item) => (
+                <div className='sellerCardComp' key={item.id}>
+                    <div className='number'>
+                        <p>0 {item.id}</p>
+                    </div>
+                    <div className='profile'>
+                        <img src={item.image} alt='' />
+                        <div className='name'>
+                            <h3>{item.name}</h3>
+                            <p>{item.currency}</p>
+                        </div>
+                    </div>
+                </div>
+            ))}
         </SellerCardsStyled>
     )
 }
 
 const SellerCardsStyled = styled.div`
+    border-radius:20px;
+    background: rgba(255,255,255,0.03);
+    transition:all 0.4s ease-in-out;
 
+    &:hover{
+        transform:translateY(-10%);
+    }
+
+    .sellerCardComp{
+        display:flex;
+        align-items:center;
+        padding:1.5rem 1rem;
+
+        .number{
+            padding-right:1rem;
+            font-size:1.6rem;
+            opacity:0.5;
+            
+            p{
+                font-weight:700;
+            }
+        }
+
+        .profile{
+            display:flex;
+
+            img{
+                width:70px;
+                height:70px;
+                border-radius:50%;
+                margin-right:1rem;
+                border:2px solid #395FF6;
+                object-fit:cover;
+            }
+        }
+        
+    }
 `
 
 export default SellerCards
